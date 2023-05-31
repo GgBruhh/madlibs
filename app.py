ce958d10acb353167d799d7ca365a1cb1216af40
Camryn Scott <camryn1242@yahoo.com>
2023-05-31 15:23:45 -0400

    initial commit + added core functionality

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from random import choice
+from stories import story
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'SECRET'
+debug = DebugToolbarExtension(app)
+
+
+@app.route('/')
+def homepage():
+    prompts = story.prompts
+    return render_template('home.html', prompts=prompts)
+
+@app.route('/story')
+def story_page():
+    text = story.generate(request.args)
+    return render_template('story.html', text=text)
