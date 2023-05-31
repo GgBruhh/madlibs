8bd45b86977b74fba8b85d11f276be148a1cd6db
Camryn Scott <camryn1242@yahoo.com>
2023-05-31 16:28:51 -0400

    added drop down box with multiple stories to pick from

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 from random import choice
-from stories import story
+from stories import stories
 
 
 app = Flask(__name__)
@@ -12,10 +12,24 @@ debug = DebugToolbarExtension(app)
 
 @app.route('/')
 def homepage():
+    return render_template('select.html', stories=stories.values())
+
+   
+
+@app.route('/form')
+def ask_questions():
+    story_id = request.args["story_id"]
+    story = stories[story_id]
     prompts = story.prompts
-    return render_template('home.html', prompts=prompts)
+    return render_template("form.html", story_id=story_id, title=story.title, prompts=prompts)
+
 
 @app.route('/story')
 def story_page():
+    
+    story_id = request.args["story_id"]
+    story = stories[story_id]
+
     text = story.generate(request.args)
-    return render_template('story.html', text=text)
+
+    return render_template("story.html", title=story.title, text=text)
